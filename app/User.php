b623c31a8da5ed4cab0de374f505fa4ab782e5f5
Wilson Gurung <wilsongurung12@gmail.com>
2018-12-30 11:59:39 +0545

    Prepapulate users and question tables using model factories and seed

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function Questions(){
-        return $this->hasMany(Questions::class);
+    public function questions(){
+        return $this->hasMany(Question::class);
     }
 }
