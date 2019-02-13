6ec7e483d1a3a19dcf2807e28e5a39c8b75fb41d
Wilson Gurung <wilsongurung12@gmail.com>
2019-02-13 13:44:17 +0545

    Implement delete functionality for the question

--- app/Question.php
+++ app/Question.php
@@ -18,7 +18,7 @@ class Question extends Model
     }
 
     public function getUrlAttribute(){
-        return route("questions.show", $this->id);
+        return route("questions.show", $this->slug);
     }
 
     public function getCreatedDateAttribute() {
@@ -35,6 +35,10 @@ class Question extends Model
 
         return "unanswered";
 
+    }
+
+    public function getBodyHtmlAttribute() {
+        return \Parsedown::instance()->text($this->body);
     }
 }
 
